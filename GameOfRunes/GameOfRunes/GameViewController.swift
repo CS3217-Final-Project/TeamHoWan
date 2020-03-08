c28da46d106874ab917cee3a5cefcc9ea17c86f1
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-03-08 14:39:01 +0800

    Set up pods and swiftlint

---
+++ GameOfRunes/GameOfRunes/GameViewController.swift
@@ -0,0 +1,60 @@
+//
+//  GameViewController.swift
+//  GameOfRunes
+//
+//  Created by Jermy on 8/3/20.
+//  Copyright © 2020 TeamHoWan. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        // including entities and graphs.
+        if let scene = GKScene(fileNamed: "GameScene") {
+            
+            // Get the SKScene from the loaded GKScene
+            if let sceneNode = scene.rootNode as? GameScene {
+                
+                // Copy gameplay related content over to the scene
+                sceneNode.entities = scene.entities
+                sceneNode.graphs = scene.graphs
+                
+                // Set the scale mode to scale to fit the window
+                sceneNode.scaleMode = .aspectFill
+                
+                // Present the scene
+                if let view = self.view as? SKView {
+                    view.presentScene(sceneNode)
+                    
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
+            }
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        true
+    }
+}
---
+++ GameOfRunes/Pods/Target Support Files/Pods-GameOfRunes/Pods-GameOfRunes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GameOfRunes : NSObject
+@end
+@implementation PodsDummy_Pods_GameOfRunes
+@end
