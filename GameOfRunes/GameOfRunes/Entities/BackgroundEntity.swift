9f75e5b349a2030699dc64a03f245af96a8424ea
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-03-08 21:25:01 +0800

    Add background and enemy images

--- GameOfRunes/GameOfRunes/Entities/BackgroundEntity.swift
+++ GameOfRunes/GameOfRunes/Entities/BackgroundEntity.swift
@@ -1,5 +1,5 @@
 //
-//  EnemyEntity.swift
+//  BackgroundEntity.swift
 //  GameOfRunes
 //
 //  Created by Jermy on 8/3/20.
@@ -9,19 +9,12 @@
 import SpriteKit
 import GameplayKit
 
-class EnemyEntity: GKEntity {
+class BackgroundEntity: GKEntity {
     
     init(imageName: String) {
         super.init()
         
         let spriteComponent = SpriteComponent(texture: SKTexture(imageNamed: imageName))
-        addComponent(spriteComponent)
-    }
-    
-    init(texture: SKTexture) {
-        super.init()
-        
-        let spriteComponent = SpriteComponent(texture: texture)
         addComponent(spriteComponent)
     }
     
--- GameOfRunes/GameOfRunes/GameScene.swift
+++ GameOfRunes/GameOfRunes/GameScene.swift
@@ -10,101 +10,45 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    var entityManager: EntityManager!
-    
-    var entities = [GKEntity]()
-    var graphs = [String: GKGraph]()
-    
-    private var lastUpdateTime: TimeInterval = 0
-    private var label: SKLabelNode?
-    private var spinnyNode: SKShapeNode?
+    private var entityManager: EntityManager!
     
     override func sceneDidLoad() {
         entityManager = .init(scene: self)
         
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = .init(rectOf: .init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        setUpBackground()
+        setUpEnemy()
     }
     
-    func touchDown(atPoint pos: CGPoint) {
-        if let n = self.spinnyNode?.copy() as? SKShapeNode {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos: CGPoint) {
-        if let n = self.spinnyNode?.copy() as? SKShapeNode {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos: CGPoint) {
-        if let n = self.spinnyNode?.copy() as? SKShapeNode {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction(named: "Pulse")!, withKey: "fadeInOut")
+    private func setUpBackground() {
+        let backgroundEntity = BackgroundEntity(imageName: "arena 2")
+        
+        if let spriteComponent = backgroundEntity.component(ofType: SpriteComponent.self) {
+            spriteComponent.node.position = .init(
+                x: size.width / 2,
+                y: size.height / 2
+            )
+            spriteComponent.node.size = size
+            spriteComponent.node.zPosition = -1
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        entityManager.add(backgroundEntity)
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    private func setUpEnemy() {
+        let evilKnightAtlas = SKTextureAtlas(named: "Evil Knight")
+        let enemyEntity = EnemyEntity(texture: evilKnightAtlas.textureNamed("WALK_002"))
         
-        // Initialize _lastUpdateTime if it has not already been
-        if self.lastUpdateTime == 0 {
-            self.lastUpdateTime = currentTime
+        if let spriteComponent = enemyEntity.component(ofType: SpriteComponent.self) {
+            spriteComponent.node.position = .init(
+                x: size.width / 2,
+                y: size.height / 2
+            )
+            let currentSpriteSize = spriteComponent.node.size
+            let newSpriteWidth = size.width / 8
+            let newSpriteHeight = currentSpriteSize.height / currentSpriteSize.width * newSpriteWidth
+            spriteComponent.node.size = .init(width: newSpriteWidth, height: newSpriteHeight)
         }
         
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+        entityManager.add(enemyEntity)
     }
 }
