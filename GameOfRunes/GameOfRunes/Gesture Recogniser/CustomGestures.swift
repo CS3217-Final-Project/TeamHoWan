08236905ecdb4f54d7ac61da20ec932d8e6f42ff
Andy <andyylam1997@gmail.com>
2020-03-17 00:30:40 +0800

    Removed diamond

--- GameOfRunes/GameOfRunes/Gesture Recogniser/CustomGestures.swift
+++ GameOfRunes/GameOfRunes/Gesture Recogniser/CustomGestures.swift
@@ -12,12 +12,9 @@ enum CustomGesture: String, CaseIterable {
     case verticalLine
     case arrowUp
     case arrowLeft
-    case bShape
     case contortedCShape
     case pShape
-    case capitalFShape
     case lightning
-    case diamond
     case ribbon
     
     func getPathModel() -> PathModel {
@@ -29,37 +26,33 @@ enum CustomGesture: String, CaseIterable {
                 (cost:Int, infos:PathInfos) -> Int in
                 guard let first = infos.deltaPoints.first?.y,
                     let last = infos.deltaPoints.last?.y else {
-                    return cost
+                        return cost
                 }
                 if (last - first > Int16(0.5 * Double(infos.height))) {
-                    return cost + 2
+                    return cost + 3
+                }
+                if infos.deltaPoints.count > 3 {
+                    let thirdLast = infos.deltaPoints[infos.deltaPoints.count - 3].y
+                    if (last <= thirdLast) {
+                        return cost + 3
+                    }
                 }
                 return cost
             })
-        case .capitalFShape:
-            return PathModel(directions: [4, 3, 2, 1, 0, 4, 3, 2], datas: self as AnyObject)
         case .arrowLeft:
-            return PathModel(directions: [3, 1], datas: self as AnyObject)
-        case .bShape:
-            return PathModel(directions: [2,6,0,1,2,3,4,0,1,2,3,4], datas: self as AnyObject)
+            return PathModel(directions: [4, 3, 0, 1], datas: self as AnyObject)
         case .lightning:
             return PathModel(directions: [2, 7, 2], datas: self as AnyObject, filter:{
                 (cost:Int, infos:PathInfos) -> Int in
                 guard let first = infos.deltaPoints.first?.y,
                     let last = infos.deltaPoints.last?.y else {
-                    return cost
+                        return cost
                 }
                 if (last - first < Int16(0.8 * Double(infos.height))) {
                     return cost + 5
                 }
                 return cost
             })
-        case .diamond:
-            return PathModel(directions: [1, 3, 5, 7], datas: self as AnyObject)
-        case .ribbon:
-            return PathModel(directions: [7, 5, 3, 1], datas: self as AnyObject)
-        case .contortedCShape:
-            return PathModel(directions: [3, 5, 2, 7, 1], datas: self as AnyObject)
         case .pShape:
             return PathModel(directions: [6, 1, 3, 3], datas: self as AnyObject, filter:{
                 (cost:Int, infos:PathInfos) -> Int in
@@ -73,6 +66,10 @@ enum CustomGesture: String, CaseIterable {
                 }
                 return cost
             })
+        case .ribbon:
+            return PathModel(directions: [7, 5, 3, 1], datas: self as AnyObject)
+        case .contortedCShape:
+            return PathModel(directions: [3, 5, 2, 7, 1], datas: self as AnyObject)
         }
     }
     
