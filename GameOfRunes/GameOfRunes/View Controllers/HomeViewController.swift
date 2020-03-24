c395dd109053d19ea7a7f83f299d4caf7040c110
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-03-25 01:08:06 +0800

    Add game assets

---
+++ GameOfRunes/GameOfRunes/View Controllers/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  GameOfRunes
+//
+//  Created by Jermy on 25/3/20.
+//  Copyright © 2020 TeamHoWan. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
