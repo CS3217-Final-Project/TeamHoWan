969dfeaa576f30a04f8af9a67b9430e1a7f0dffa
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-03-25 03:33:15 +0800

    Add home screen with button navigation

--- GameOfRunes/GameOfRunes/View Controllers/HomeViewController.swift
+++ GameOfRunes/GameOfRunes/View Controllers/HomeViewController.swift
@@ -10,21 +10,93 @@ import UIKit
 import SnapKit
 
 class HomeViewController: UIViewController {
+    var viewPortWidth: CGFloat {
+        view.frame.size.width
+    }
+    var viewPortHeight: CGFloat {
+        view.frame.size.height
+    }
+    let startButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUpHomeBackground()
+        setUpStartButton()
     }
     
     private func setUpHomeBackground() {
         let background = UIImageView(image: UIImage(named: "relic-guardian"))
-        background.clipsToBounds = true
         background.contentMode = .scaleAspectFill
         
         view.addSubview(background)
         background.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.edges.equalToSuperview().labeled("backgroundEdges")
         }
+    }
+    
+    private func setUpStartButton() {
+        view.addSubview(startButton)
+        view.bringSubviewToFront(startButton)
+        addImageConstraints()
+        addTitle()
+        addActions()
+    }
+}
+
+// MARK: - button setup
+extension HomeViewController {
+    private func addImageConstraints() {
+        guard let buttonImage = UIImage(named: "start-button") else {
+            return
+        }
+        
+        let buttonWidth = viewPortWidth / 1.25
+        let buttonHeight = buttonWidth / buttonImage.size.widthToHeightRatio
+        
+        startButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview().labeled("startButtonCenterX")
+            make.centerY.equalToSuperview().multipliedBy(1.5).labeled("startButtonCenterY")
+            make.width.equalTo(buttonWidth).labeled("startButtonWidth")
+            make.height.equalTo(buttonHeight).labeled("startButtonHeight")
+        }
+        
+        startButton.adjustsImageWhenHighlighted = false
+        startButton.setBackgroundImage(buttonImage, for: .normal)
+    }
+    
+    private func addTitle() {
+    }
+    
+    private func addActions() {
+        startButton.addTarget(self, action: #selector(onTapped), for: .touchDown)
+        startButton.addTarget(self, action: #selector(onTouchedUpOutside), for: .touchUpOutside)
+        startButton.addTarget(self, action: #selector(onSelectedStart), for: .touchUpInside)
+    }
+    
+    @objc private func onTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.05) {
+            sender.transform = CGAffineTransform(scaleX: 0.90, y: 0.88)
+        }
+    }
+    
+    @objc private func onTouchedUpOutside(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.1) {
+            sender.transform = CGAffineTransform.identity
+        }
+    }
+    
+    @objc private func onSelectedStart(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.1) {
+            sender.transform = CGAffineTransform.identity
+        }
+        
+        guard let gameVC = storyboard?.instantiateViewController(identifier: "GameVC") else {
+            return
+        }
+        
+        gameVC.modalPresentationStyle = .fullScreen
+        gameVC.modalTransitionStyle = .crossDissolve
+        present(gameVC, animated: true)
     }
 }
