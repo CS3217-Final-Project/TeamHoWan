a22a877b8a95a75c3447ec8264fc6d9afa262c40
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-03-25 12:59:11 +0800

    Add texture loading in tests

--- GameOfRunes/GameOfRunesTests/GameEngineTest.swift
+++ GameOfRunes/GameOfRunesTests/GameEngineTest.swift
@@ -14,6 +14,7 @@ import GameplayKit
 class GameEngineTest: BaseUnitTest {
     override func setUp() {
         super.setUp()
+        TextureContainer.loadTextures()
         stub(gameEngine) { stub in
             when(stub.playerHealthEntity.get).thenReturn(playerHealthEntity)
             when(stub.playerManaEntity.get).thenReturn(playerManaEntity)
