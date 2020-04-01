6a202c0ce990dcd88d856a1a755391d4f361ee76
Andy <andyylam1997@gmail.com>
2020-04-01 09:14:49 +0800

    [NEW] Added firebase pod

--- GameOfRunes/GameOfRunes/AppDelegate.swift
+++ GameOfRunes/GameOfRunes/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
