85711be57cbbe3a0c1f76ebc196f1ef0983149e7
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-04-14 04:06:49 +0800

    Add regex to fix edge cases for player name

--- GameOfRunes/GameOfRunes/Scenes/GameHomeScene.swift
+++ GameOfRunes/GameOfRunes/Scenes/GameHomeScene.swift
@@ -136,18 +136,20 @@ extension GameHomeScene: UITextFieldDelegate {
         shouldChangeCharactersIn range: NSRange,
         replacementString string: String
     ) -> Bool {
-        if string.isEmpty {
+        if string.isEmpty || string == " " {
             return true
         } else if (textField.text ?? "").count > 12 {
             return false
         } else {
-            let invalidChars = CharacterSet(charactersIn: ";,.-+=*/")
-            return string.removingCharacters(in: invalidChars) == string
+            // ensures all characters are alphanumerics
+            return string.removingCharacters(in: .alphanumerics).isEmpty
         }
     }
     
     func textFieldDidChangeSelection(_ textField: UITextField) {
-        playerName = textField.text ?? ""
+        let processedText = (textField.text ?? "").replacingOccurrences(of: "^\\s+", with: "", options: .regularExpression)
+        playerName = processedText.trimmingCharacters(in: .whitespacesAndNewlines)
+        textField.text = processedText
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
