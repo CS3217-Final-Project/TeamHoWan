46a9fb0de2e8f1ec050f2b71ac6956a9aaf87c1c
Brian Yen <brianyen.na@gmail.com>
2020-04-21 23:41:02 +0800

    Refactor getManaType into ManaType

--- GameOfRunes/GameOfRunes/Types/ManaType.swift
+++ GameOfRunes/GameOfRunes/Types/ManaType.swift
@@ -10,4 +10,20 @@ enum ManaType: String, CaseIterable {
     case common
     case rare
     case ultraRare
+
+    static func getManaType(manaPoints: Int) -> ManaType {
+        let max = GameConfig.Mana.manaMaxValue
+        let min = GameConfig.Mana.manaMinValue
+
+        let percentile = Double((manaPoints - min)) / Double((max - min)) * 100
+
+        switch percentile {
+        case GameConfig.Mana.manaUltraRareThreshold...:
+            return .ultraRare
+        case GameConfig.Mana.manaRareThreshold...:
+            return .rare
+        default:
+            return .common
+        }
+    }
 }
